ed7d8d8a1f31d51ce0812b8e7cdd871372a5ac04
krentz <r4f4_kr3ntz@hotmail.com>
2020-07-16 20:14:00 +0100

    first commit

---
+++ StudyProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyProject
+//
+//  Created by Rafael Krentz Gonçalves on 7/16/20.
+//  Copyright © 2020 krentz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
